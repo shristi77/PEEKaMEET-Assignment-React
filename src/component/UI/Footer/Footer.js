385d7ddf3cf0c4fca84af8443032c4442eb254d7
Shristi Gupta <shristi.gupta@unthinkable.co>
2020-11-07 14:19:30 +0530

    signIn page footer css added

--- src/component/UI/Footer/Footer.js
+++ src/component/UI/Footer/Footer.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import classes from './Footer.css';
-import Image from '../../UI/Image/Image';
+import React from "react";
+import classes from "./Footer.css";
+import facebook from "../../../assests/SocialMediaLogo/Facebook/facebook-box-fill.png";
+import instagram from "../../../assests/SocialMediaLogo/Instagram/instagram-fill.png";
+import twitter from "../../../assests/SocialMediaLogo/Twitter/twitter-fill.png";
+import youtube from "../../../assests/SocialMediaLogo/Youtube/youtube-fill.png";
 
 const Footer = (props) => {
-
-    return (
-        <div>
-            <footer className={[classes.Footer, classes[props.footerType]].join(' ')}>
-                <span className={classes.peekameet}>PEEKaMEET&copy;2020</span>
-                <span className={classes.follow}>Follow Us:</span>
-                <Image type="footer"/>
-                <span>FAQs </span>
-                <span>Terms &amp; Conditions</span>
-                <span>Privacy Policy </span>
-                <span>About Us </span>
-                <span>Press </span>
-                <span>Contact Us </span>
-                <span>Perks </span>
-                <span>Blog </span>
-            </footer>
+  return (
+    <div>
+      <footer className={classes.Footer}>
+        <div className={classes.logoText}>PEEKaMEET&copy;2020</div>
+        <div>Follow Us: </div>
+        <div className={classes.Img}>
+          <img src={instagram} alt={instagram}></img>
         </div>
-    )
-}
+        <div className={classes.Img}>
+          <img src={youtube} alt={youtube}></img>
+        </div>
+        <div className={classes.Img}>
+          <img src={facebook} alt={facebook}></img>
+        </div>
+        <div className={classes.Img}>
+          <img src={twitter} alt={twitter}></img>
+        </div>
+        <div className={classes.links}>FAQs</div>
+        <div className={classes.links}>Terms &amp; Conditions</div>
+        <div className={classes.links}>Privacy Policy</div>
+        <div className={classes.links}>About Us</div>
+        <div className={classes.links}>Press</div>
+        <div className={classes.links}>Contact Us</div>
+        <div className={classes.links}>Perks</div>
+        <div className={classes.links}>Blog</div>
+      </footer>
+    </div>
+  );
+};
 
 export default Footer;
