1668f9a43993914ed77d28a2dbc65f108ee23529
Shristi Gupta <shristi.gupta@unthinkable.co>
2020-11-07 14:56:14 +0530

    added css to Input component

--- src/component/UI/Input/Input.js
+++ src/component/UI/Input/Input.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import classes from './Input.css';
+import React from "react";
+import classes from "./Input.css";
 
 const Input = (props) => {
-    const inputClasses = [classes.InputElement];
+  const inputClasses = [classes.InputElement];
 
-    return (
-        <div className={classes.Input}>
-        <label className={classes.Label}>{props.inputLabel}</label>
-        <input className={inputClasses.join(' ')} type={props.inputType} value={props.inputValue} onChange={props.changed} />
-    </div>  
-    );
-}
+  return (
+    <div className={classes.Input}>
+      <label className={classes.Label}>{props.inputLabel}</label>
+      <input
+        className={inputClasses.join(" ")}
+        type={props.inputType}
+        value={props.inputValue}
+        onChange={props.changed}
+      />
+    </div>
+  );
+};
 
 export default Input;
