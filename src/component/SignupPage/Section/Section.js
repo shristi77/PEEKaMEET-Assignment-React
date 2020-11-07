36bdd627add9c237b3428185a65186e158938587
Shristi Gupta <shristi.gupta@unthinkable.co>
2020-11-07 14:42:12 +0530

    colourfull rectangles css added in signIn page

--- src/component/SignupPage/Section/Section.js
+++ src/component/SignupPage/Section/Section.js
@@ -1,47 +1,45 @@
-import React from 'react';
-import classes from './Section.css';
-import Image from '../../UI/Image/Image';
-import SignupSection from './SignupSection/SignupSection';
-import Element from '../../UI/Elements/Element';
+import React from "react";
+import classes from "./Section.css";
+import Image from "../../UI/Image/Image";
+import SignupSection from "./SignupSection/SignupSection";
+import Element from "../../UI/Elements/Element";
 
 const Section = (props) => {
-
-    const rectangles = [
-        { text: 'Freelancer', color: 'Orange' },
-        { text: 'Job Seeker', color: 'Purple' },
-        { text: 'Enterpreneur', color: 'Green' },
-        { text: 'Mompreneur', color: 'Pink' },
-        { text: 'Internship Seeker', color: 'Blue' },
-        { text: 'Environmental Change Maker', color: 'Red' }
-    ]
-    return (
-        <div> 
-            <section className={classes.Section}>
-                <div>
-                    <Image type="signupHeader"/>
-                </div>
-                <div className={classes.section2}>
-                    <p className={classes.para1}>
-                        <span 
-                            className={classes.peekameet}>
-                                PEEKaMEET
-                        </span> lets you network more effectively to achieve your business and career goals
-                    </p>
-                    <div>
-                        {rectangles.map(element => 
-                            <Element key={element.text} Color={element.color}>{element.text}</Element>
-                        )}
-                    </div>
-                    <p className={classes.para2}>
-                    Build and manage your network with PEEKaMEET
-                    </p>
-                    <SignupSection/>
-                    {/* <input type="checkbox">Remember me</input> */}
-                </div>
-            </section>
+  const rectangles = [
+    { text: "Freelancer", color: "Orange" },
+    { text: "Job Seeker", color: "Purple" },
+    { text: "Enterpreneur", color: "Green" },
+    { text: "Mompreneur", color: "Pink" },
+    { text: "Internship Seeker", color: "Blue" },
+    { text: "Environmental Change Maker", color: "Red" },
+  ];
+  return (
+    <div>
+      <section className={classes.Section}>
+        <div>
+          <Image type="signupHeader" />
         </div>
-    )
-}
-
+        <div className={classes.section2}>
+          <p className={classes.para1}>
+            <span className={classes.peekameet}>PEEKaMEET</span> lets you
+            network more effectively to achieve your business and career goals
+          </p>
+          <div className={classes.container}>
+            {rectangles.map((element) => (
+              <Element key={element.text} Color={element.color}>
+                {element.text}
+              </Element>
+            ))}
+          </div>
+          <p className={classes.para2}>
+            Build and manage your network with PEEKaMEET
+          </p>
+          <SignupSection />
+          {/* <input type="checkbox">Remember me</input> */}
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Section;
--- src/component/UI/Elements/Element.js
+++ src/component/UI/Elements/Element.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import classes from './Element.css';
+import React from "react";
+import classes from "./Element.css";
 
 const Element = (props) => {
-    return (
-    <div 
-        className={[classes.Rectangle, classes[props.Color]].join(' ')}>
-            {props.children}
+  return (
+    <div className={[classes.Rectangle, classes[props.Color]].join(" ")}>
+      {props.children}
     </div>
-    )
-}
+  );
+};
 
-export default Element
+export default Element;
